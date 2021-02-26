40324ab53cdf306dba3161be863f6e19809f7c4a
Matthieu <Gdon>
2021-02-26 18:18:21 +0100

    fix: Comments on part one

--- part-one/app/src/main/java/com/example/matthieugedeon/tp2/MainActivity.java
+++ part-one/app/src/main/java/com/example/matthieugedeon/tp2/MainActivity.java
@@ -30,11 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Method that launch the authentication thread
+    //Launched when clicking on the button authenticate (linking done in the activity_main.xml file)
     public void authenticate(View v){
         authenticateThread t = new authenticateThread();
         t.start();
     }
 
+    //Function found on StackOverflow
+    //Construct a string from data extracted of a Stream
     private String readStream(InputStream is) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
@@ -45,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         return sb.toString();
     }
 
+    //Parameterized Runnable that will be passed to the runOnUIThread to update the view
     class parameterizedRunnable{
         boolean res;
         Runnable runnable;
@@ -61,30 +66,41 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Activity logic based in a thread
     class authenticateThread extends Thread {
         public void run(){
             URL url = null;
             EditText field1 = (EditText) findViewById(R.id.username_field);
             EditText field2 = (EditText) findViewById(R.id.password_field);
 
-
+            //User input credentials concatenated
             String authConcat = field1.getText().toString() + ":" + field2.getText().toString();
             try {
+
+                //Url to the authentication service Bob:sympa
                 url = new URL("https://httpbin.org/basic-auth/bob/sympa");
 
+                //HttpURLConnection object supporting HTTP-based basic features
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
+                //Data encoder fo the request
                 String basicAuth = "Basic " + Base64.encodeToString(authConcat.getBytes(),
                         Base64.NO_WRAP);
+
+                //Setting the request parameters (key, value)
                 urlConnection.setRequestProperty ("Authorization", basicAuth);
 
                 try {
+                    //Getting the InputStream from the Url connection in order to get data
                     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                    //Getting the data from the InputStream thanks to the readStream method
                     String s = readStream(in);
                     Log.i("JFL", s);
 
                     JSONObject result;
                     try{
+                        //Constructing a JSONObject from the fetched string
                         result = new JSONObject(s);
                     }
                     catch(Exception e){
@@ -93,12 +109,14 @@ public class MainActivity extends AppCompatActivity {
 
                     parameterizedRunnable pr;
                     try {
+                        //Instantiate a parameterizedRunnable for the runOnUIThread
                         pr = new parameterizedRunnable(result.getBoolean("authenticated"));
                     }
                     catch (Exception e){
                         pr = new parameterizedRunnable(false);
                     }
 
+                    //Method to update the view
                     runOnUiThread(pr.runnable);
                 } finally {
                     urlConnection.disconnect();
@@ -110,21 +128,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-    /*
-    private String readStream(InputStream is) {
-        try {
-            ByteArrayOutputStream bo = new ByteArrayOutputStream();
-            int i = is.read();
-            while(i != -1) {
-                bo.write(i);
-                i = is.read();
-            }
-            return bo.toString();
-        } catch (IOException e) {
-            return "";
-        }
-    }
-    */
 
 }
